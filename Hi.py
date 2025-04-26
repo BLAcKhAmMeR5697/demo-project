338515dbd65287565e68b6cfcff7a92a314b1f15
BLAcKhAmMeR5697 <guruduttadke@gmail.com>
2025-04-26 18:13:03 +0530

    Main

---
+++ Hi.py
@@ -0,0 +1,13 @@
+# Define a function called "output_greetings"
+def output_greetings(greetings):
+    # Iterate over each greeting in the list
+    for greeting in greetings:
+        # Print the greeting message
+        print(greeting)
+
+
+# Create a variable with a list of greetings
+greetings = ["Hello", "Hola", "Bonjour", "Guten Tag", "Nǐn Hǎo"]
+
+# Call the say_hello function to execute its commands
+output_greetings(greetings)
